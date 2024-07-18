04d0cd62f3ed3494042c8206b2ce0035b9304a32
mourya <mouryavardhanbk711@gmail.com>
2024-07-18 18:24:33 +0530

    Added hangman game and modified readme

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python-Hangman-Game
 A simple CLI hangman game implemented using python
+The user has 6 chances to guess the movie name correctly
---
+++ hangman.py
@@ -0,0 +1,135 @@
+import random
+
+def print_hangman(tries):
+    """Prints the hangman based on the number of incorrect tries."""
+
+    stages = [# initial empty state, mistake = 0
+                """
+                   --------
+                   |      
+                   |    
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # head, mistake = 1``
+                """
+                   --------
+                   |      
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # head and torso, mistake = 2
+                """
+                   --------
+                   |      
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head, torso, and one arm, mistake = 3
+                """
+                   --------
+                   |      
+                   |      O
+                   |    \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head, torso, and both arms, mistake = 4
+                """
+                   --------
+                   |      
+                   |      O
+                   |    \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                # head, torso, both arms, and one leg, mistake = 5
+                """
+                   --------
+                   |      
+                   |      O
+                   |    \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # final state: head, torso, both arms, and both legs, mistake = 6
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    \\|/
+                   |      |
+                   |     / \\
+                   -
+                """
+    ]
+    print(stages[tries])
+
+def game_start(guess):
+    print("THE GAME STARTS!!!")
+    print("guess the movie:")
+    # print(guess)
+    unknown = ["_"]*len(guess)
+    mistakes = 0
+    life = 6
+    alphabets = set()
+    
+    while "_" in unknown and mistakes < 6:
+        check = False #Flag variable
+        print("No. of spelling to find:",unknown)
+        user_input = input("guess one letter:").lower()
+    
+        if user_input in alphabets:
+            print("You have already entered that letter, choose different one")
+            print()
+
+        else:
+            alphabets.add(user_input)
+            
+            for i in range(0,len(guess)):
+                if guess[i] == user_input:
+                    unknown[i] = user_input
+                    check = True
+                
+            if check == True:
+                print("hooray!!, You have guessed correct")
+                print("Remaining LIVES:",life)
+                print()
+
+            else:
+                print("You have guessed wrong... try again")
+                print("YOU HAVE LOST A LIFE")
+                mistakes += 1
+                life -= 1
+                print("Remaining LIVES:",life)
+                print_hangman(mistakes)
+                print()
+        
+        if "_" not in unknown:
+            print("CONGRAGULATIONS, YOU HAVE WON THE GAME....")
+
+        if mistakes == 6:
+            print("GAME OVER....")          
+        
+
+movies = ["avatar","avengers","titanic","joker","kill"]
+while 1:
+    guess = random.choice(movies)
+    control = input("Enter 'start' or 'exit':").lower()
+
+    if control == "start":
+        game_start(guess)
+    else:
+        print("EXITING THE GAME...")
+        exit()
